d3a12bc2e9b1fa5dc9b5e1eb183dd3fc8d499f79
issyu-summer <2119635988@qq.com>
2021-02-28 21:27:38 +0800

    集成了nacos作为注册中心-2021/02/28

---
+++ core/src/main/java/cn/edu/xmu/core/utils/HttpUtil.java
@@ -0,0 +1,15 @@
+package cn.edu.xmu.core.utils;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+
+import java.util.HashMap;
+
+/**
+ * @author summer
+ * @date 2021/2/28 13:04
+ */
+public class HttpUtil {
+
+
+}
--- other-service/src/main/java/cn/edu/xmu/other/controller/OtherController.java
+++ other-service/src/main/java/cn/edu/xmu/other/controller/OtherController.java
@@ -1,8 +1,19 @@
 package cn.edu.xmu.other.controller;
 
+import cn.edu.xmu.core.utils.Response;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * @author summer
@@ -12,8 +23,40 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class OtherController {
 
-    @GetMapping("/other")
-    public String test(){
-        return "other will be filtered";
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/admin/login")
+    public Object test(){
+
+        ServiceInstance serviceInstance = loadBalancerClient.choose("provider");
+        String path
+                = String.format("http://%s:%s/user/login",serviceInstance.getHost(),serviceInstance.getPort());
+        Map<String,String> body = new HashMap<>();
+        body.put("username","admin");
+        body.put("password","admin");
+        Object result
+                = restTemplate.postForObject(path,new UserVo().setUsername("admin").setPassword("admin"),Response.class);
+        return result;
     }
+    @GetMapping("/test")
+    public Object other(){
+
+        ServiceInstance serviceInstance = loadBalancerClient.choose("provider");
+        String path
+                = String.format("http://%s:%s/user/test",serviceInstance.getHost(),serviceInstance.getPort());
+        Object result=restTemplate.getForObject(path,Response.class);
+        return result;
+    }
+
+    @Accessors(chain = true)
+    @Data
+    public static class UserVo{
+        private String username;
+        private String password;
+    }
+
 }
